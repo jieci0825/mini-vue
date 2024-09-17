5f0fba055fe788b548d2731442f5e17cf83aaefd
coderjc <2740954762@qq.com>
2024-09-18 02:42:01 +0800

    refactor: 优化代码

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,9 +1,10 @@
 import { ITERATE_KEY } from './constants'
+import { createDep, Dep } from './dep'
 import { TrackOpType, TriggerOpType } from './operations'
 
 let isTracking = true
 
-type KeyToDepMap = Map<any, Set<ReactiveEffect>>
+type KeyToDepMap = Map<any, Dep>
 
 const targetMap = new WeakMap<any, KeyToDepMap>()
 
@@ -46,14 +47,22 @@ export function track(target: object, type: TrackOpType, key: unknown) {
     let deps = propMap.get(prop)
     // 如果不存在，则创建一个集合
     if (!deps) {
-        deps = new Set()
+        deps = createDep()
+        // 再将属性名和依赖的函数集合关联起来
+        propMap.set(prop, deps)
     }
     // 此时在将此次收集的 activeEffect 添加到集合中
-    deps.add(activeEffect)
-    // 再将属性名和依赖的函数集合关联起来
-    propMap.set(prop, deps)
+    trackEffects(deps)
 
     // console.log('targetMap', targetMap)
+}
+
+/**
+ * 利用 dep 依次跟踪指定 key 的所有 effect
+ * @param deps
+ */
+export function trackEffects(deps: Dep) {
+    deps.add(activeEffect!)
 }
 
 /**
@@ -69,11 +78,24 @@ export function trigger(target: object, type: TriggerOpType, key: unknown, newVa
     // 根据 key 在 propMap 中获取对应的依赖集合
     const deps = propMap?.get(key)
     // 如果存在，则遍历集合，获取对应的依赖并执行
-    if (deps) {
-        deps.forEach(dep => {
-            dep.fn()
-        })
-    }
+    deps && triggerEffects(deps)
+}
+
+/**
+ * 依次触发 deps 中保存的依赖
+ */
+export function triggerEffects(deps: Dep) {
+    // 依次触发
+    deps.forEach(effect => {
+        triggerEffect(effect)
+    })
+}
+
+/**
+ * 触发指定依赖
+ */
+export function triggerEffect(effect: ReactiveEffect) {
+    effect.run()
 }
 
 /**
