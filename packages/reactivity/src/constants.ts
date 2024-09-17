acf30b936dbcf20c4483d8fc531d4476a0bc150d
coderjc <2740954762@qq.com>
2024-09-18 02:44:26 +0800

    feat: 解决代理对象再次被代理的问题

--- packages/reactivity/src/constants.ts
+++ packages/reactivity/src/constants.ts
@@ -1,2 +1,3 @@
 export const IsRaw = Symbol('__isRaw__')
+export const IsReactive = Symbol('__isReactive__')
 export const ITERATE_KEY = Symbol('iterate')
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,5 +1,6 @@
 import { isObject } from '@vue/shared'
 import { mutableHandlers } from './baseHandler'
+import { IsReactive } from './constants'
 
 export const reactiveMap = new WeakMap<object, any>()
 
@@ -12,7 +13,12 @@ function createReactiveObject(target: object, baseHandlers: ProxyHandler<any>, p
     if (!isObject(target)) {
         return target
     }
-    // 如果 target 已经被代理过，直接返回
+    // 如果 target 就是一个代理对象，则直接返回
+    if (target[IsReactive]) {
+        return target
+    }
+
+    // 如果 target 之前以前被代理过了，直接返回之前缓存的代理对象
     const existingProxy = proxyMap.get(target)
     if (existingProxy) {
         return existingProxy
@@ -21,6 +27,7 @@ function createReactiveObject(target: object, baseHandlers: ProxyHandler<any>, p
     const proxy = new Proxy(target, baseHandlers)
     // 将代理对象和 target 存储到 proxyMap 中
     proxyMap.set(target, proxy)
+    proxy[IsReactive] = true
     // 返回代理对象
     return proxy
 }
