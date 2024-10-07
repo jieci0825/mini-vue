7936815ca278881e8d64aa58ddf38de3d805a079
coderjc <2740954762@qq.com>
2024-10-08 02:30:06 +0800

    feat: 增加 vue-compat 模块

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,5 +1,6 @@
 export { reactive, effect, isReactive, ref, isRef, computed } from '@vue/reactivity'
 export { watch, queuePreFlushCbs, h, createVNode, Fragment, Text, Comment, createElementVNode } from '@vue/runtime-core'
 export { render } from '@vue/runtime-dom'
-export { compile } from '@vue/compiler-dom'
+// export { compile } from '@vue/compiler-dom'
+export { compile } from '@vue/vue-compat'
 export * from '@vue/shared'
